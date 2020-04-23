bde17c1e3f1db07fe95867421ca6dcbdf586d062
Zoilo Mora <zoilo.mora@hotmail.com>
2020-04-23 17:08:05 +0200

    Remove complexity to GroupBySpanTypes

--- src/Processor/MetricSetProcessor/GroupBySpanTypes.php
+++ src/Processor/MetricSetProcessor/GroupBySpanTypes.php
@@ -12,6 +12,43 @@ final class GroupBySpanTypes
      */
     public function execute($transactionId, array $spans)
     {
+        return $this->spanFromGroupings(
+            $transactionId,
+            $this->getGroupings($spans)
+        );
+    }
+
+    /**
+     * @param string $transactionId
+     * @param array $groupings
+     *
+     * @return Span[]
+     */
+    private function spanFromGroupings($transactionId, array $groupings)
+    {
+        $result = [];
+        foreach ($groupings as $type => $subtypes) {
+            foreach ($subtypes as $subtype => $data) {
+                $result[] = new Span(
+                    $transactionId,
+                    $type,
+                    $this->getSubType($subtype),
+                    $data['count'],
+                    $data['sum']
+                );
+            }
+        }
+
+        return $result;
+    }
+
+    /**
+     * @param Span[] $spans
+     *
+     * @return array
+     */
+    private function getGroupings(array $spans)
+    {
         $groupings = [];
 
         foreach ($spans as $span) {
@@ -33,20 +70,7 @@ final class GroupBySpanTypes
             $groupings[$type][$subType]['sum'] = $groupings[$type][$subType]['sum'] + $span->sum();
         }
 
-        $result = [];
-        foreach ($groupings as $type => $subtypes) {
-            foreach ($subtypes as $subtype => $data) {
-                $result[] = new Span(
-                    $transactionId,
-                    $type,
-                    $this->getSubType($subtype),
-                    $data['count'],
-                    $data['sum']
-                );
-            }
-        }
-
-        return $result;
+        return $groupings;
     }
 
     /**
