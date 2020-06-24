bef5d83abd78a28a51abc9609c9d0fccb3d33596
Zoilo Mora <zoilo.mora@hotmail.com>
2020-06-24 20:06:54 +0200

    Version change for Release

--- src/Events/Common/Service/Agent.php
+++ src/Events/Common/Service/Agent.php
@@ -7,7 +7,7 @@ use ZoiloMora\ElasticAPM\Helper\Encoding;
 final class Agent implements \JsonSerializable
 {
     const NAME = 'zoilomora/elastic-apm-agent-php';
-    const VERSION = '0.1.15';
+    const VERSION = '0.1.16';
 
     /**
      * Name of the Elastic APM agent, e.g. "Python"
